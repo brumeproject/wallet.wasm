1157fc8574a6263f3d0b8f175bdf786dc7560771
hazae41 <4405263+hazae41@users.noreply.github.com>
2025-07-14 23:03:09 +0900

    clean

--- src/wasm/pkg/wallet_wasm.d.ts
+++ src/wasm/pkg/wallet_wasm.d.ts
@@ -7,14 +7,6 @@ export function base16_decode_mixed(text: string): Memory;
 export function base16_decode_lower(text: string): Memory;
 export function base16_decode_upper(text: string): Memory;
 export function keccak256(data: Memory): Memory;
-export function base64_encode_padded(bytes: Memory): string;
-export function base64_decode_padded(text: string): Memory;
-export function base64_encode_unpadded(bytes: Memory): string;
-export function base64_decode_unpadded(text: string): Memory;
-export function base64url_encode_padded(bytes: Memory): string;
-export function base64url_decode_padded(text: string): Memory;
-export function base64url_encode_unpadded(bytes: Memory): string;
-export function base64url_decode_unpadded(text: string): Memory;
 export function ripemd160(data: Memory): Memory;
 export function base58_encode(bytes: Memory): string;
 export function base58_decode(text: string): Memory;
@@ -29,36 +21,6 @@ export class ChaCha20Poly1305Cipher {
   constructor(key: Memory);
   encrypt(message: Memory, nonce: Memory): Memory;
   decrypt(message: Memory, nonce: Memory): Memory;
-}
-export class Ed25519Signature {
-  [Symbol.dispose](): void;
-  constructor(bytes: Memory);
-  static from_bytes(bytes: Memory): Ed25519Signature;
-  to_bytes(): Memory;
-  r_bytes(): Memory;
-  s_bytes(): Memory;
-}
-export class Ed25519SigningKey {
-  [Symbol.dispose](): void;
-  constructor();
-  static random(): Ed25519SigningKey;
-  static from_bytes(bytes: Memory): Ed25519SigningKey;
-  static from_keypair_bytes(bytes: Memory): Ed25519SigningKey;
-  to_bytes(): Memory;
-  to_keypair_bytes(): Memory;
-  verifying_key(): Ed25519VerifyingKey;
-  sign(bytes: Memory): Ed25519Signature;
-  verify(bytes: Memory, signature: Ed25519Signature): boolean;
-  verify_strict(bytes: Memory, signature: Ed25519Signature): boolean;
-}
-export class Ed25519VerifyingKey {
-  [Symbol.dispose](): void;
-  constructor(bytes: Memory);
-  static from_bytes(bytes: Memory): Ed25519VerifyingKey;
-  is_weak(): boolean;
-  to_bytes(): Memory;
-  verify(bytes: Memory, signature: Ed25519Signature): boolean;
-  verify_strict(bytes: Memory, signature: Ed25519Signature): boolean;
 }
 export class Keccak256Hasher {
   [Symbol.dispose](): void;
@@ -139,44 +101,11 @@ export class Sha1Hasher {
   update(data: Memory): void;
   finalize(): Memory;
 }
-export class X25519PublicKey {
-  [Symbol.dispose](): void;
-  constructor(bytes: Memory);
-  static from_bytes(bytes: Memory): X25519PublicKey;
-  to_bytes(): Memory;
-}
-export class X25519SharedSecret {
-  private constructor();
-  [Symbol.dispose](): void;
-  to_bytes(): Memory;
-  was_contributory(): boolean;
-}
-export class X25519StaticSecret {
-  [Symbol.dispose](): void;
-  constructor();
-  static from_bytes(bytes: Memory): X25519StaticSecret;
-  to_bytes(): Memory;
-  diffie_hellman(other: X25519PublicKey): X25519SharedSecret;
-  to_public(): X25519PublicKey;
-}
 
 export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
 
 export interface InitOutput {
   readonly memory: WebAssembly.Memory;
-  readonly __wbg_x25519staticsecret_free: (a: number, b: number) => void;
-  readonly x25519staticsecret_random: () => number;
-  readonly x25519staticsecret_from_bytes: (a: number) => [number, number, number];
-  readonly x25519staticsecret_to_bytes: (a: number) => number;
-  readonly x25519staticsecret_diffie_hellman: (a: number, b: number) => number;
-  readonly x25519staticsecret_to_public: (a: number) => number;
-  readonly __wbg_x25519sharedsecret_free: (a: number, b: number) => void;
-  readonly x25519sharedsecret_to_bytes: (a: number) => number;
-  readonly x25519sharedsecret_was_contributory: (a: number) => number;
-  readonly __wbg_x25519publickey_free: (a: number, b: number) => void;
-  readonly x25519publickey_from_bytes: (a: number) => [number, number, number];
-  readonly x25519publickey_to_bytes: (a: number) => number;
-  readonly x25519publickey_new: (a: number) => [number, number, number];
   readonly sha1: (a: number) => number;
   readonly __wbg_sha1hasher_free: (a: number, b: number) => void;
   readonly sha1hasher_new: () => number;
@@ -222,30 +151,11 @@ export interface InitOutput {
   readonly keccak256hasher_clone: (a: number) => number;
   readonly keccak256hasher_update: (a: number, b: number) => void;
   readonly keccak256hasher_finalize: (a: number) => number;
-  readonly __wbg_ed25519signingkey_free: (a: number, b: number) => void;
-  readonly ed25519signingkey_new: () => number;
-  readonly ed25519signingkey_from_bytes: (a: number) => [number, number, number];
-  readonly ed25519signingkey_from_keypair_bytes: (a: number) => [number, number, number];
-  readonly ed25519signingkey_to_bytes: (a: number) => number;
-  readonly ed25519signingkey_to_keypair_bytes: (a: number) => number;
-  readonly ed25519signingkey_verifying_key: (a: number) => number;
-  readonly ed25519signingkey_sign: (a: number, b: number) => number;
-  readonly ed25519signingkey_verify: (a: number, b: number, c: number) => number;
-  readonly ed25519signingkey_verify_strict: (a: number, b: number, c: number) => number;
-  readonly ed25519signingkey_random: () => number;
-  readonly __wbg_ed25519verifyingkey_free: (a: number, b: number) => void;
-  readonly ed25519verifyingkey_from_bytes: (a: number) => [number, number, number];
-  readonly ed25519verifyingkey_is_weak: (a: number) => number;
-  readonly ed25519verifyingkey_to_bytes: (a: number) => number;
-  readonly ed25519verifyingkey_verify: (a: number, b: number, c: number) => number;
-  readonly ed25519verifyingkey_verify_strict: (a: number, b: number, c: number) => number;
-  readonly ed25519verifyingkey_new: (a: number) => [number, number, number];
-  readonly __wbg_ed25519signature_free: (a: number, b: number) => void;
-  readonly ed25519signature_from_bytes: (a: number) => [number, number, number];
-  readonly ed25519signature_to_bytes: (a: number) => number;
-  readonly ed25519signature_r_bytes: (a: number) => number;
-  readonly ed25519signature_s_bytes: (a: number) => number;
-  readonly ed25519signature_new: (a: number) => [number, number, number];
+  readonly base16_encode_lower: (a: number) => [number, number];
+  readonly base16_encode_upper: (a: number) => [number, number];
+  readonly base16_decode_mixed: (a: number, b: number) => [number, number, number];
+  readonly base16_decode_lower: (a: number, b: number) => [number, number, number];
+  readonly base16_decode_upper: (a: number, b: number) => [number, number, number];
   readonly __wbg_chacha20poly1305cipher_free: (a: number, b: number) => void;
   readonly chacha20poly1305cipher_new: (a: number) => [number, number, number];
   readonly chacha20poly1305cipher_encrypt: (a: number, b: number, c: number) => [number, number, number];
@@ -254,21 +164,8 @@ export interface InitOutput {
   readonly chacha20cipher_new: (a: number, b: number) => [number, number, number];
   readonly chacha20cipher_seek: (a: number, b: number) => [number, number];
   readonly chacha20cipher_apply_keystream: (a: number, b: number) => [number, number];
-  readonly base64_encode_padded: (a: number) => [number, number];
-  readonly base64_decode_padded: (a: number, b: number) => [number, number, number];
-  readonly base64_encode_unpadded: (a: number) => [number, number];
-  readonly base64_decode_unpadded: (a: number, b: number) => [number, number, number];
-  readonly base64url_encode_padded: (a: number) => [number, number];
-  readonly base64url_decode_padded: (a: number, b: number) => [number, number, number];
-  readonly base64url_encode_unpadded: (a: number) => [number, number];
-  readonly base64url_decode_unpadded: (a: number, b: number) => [number, number, number];
   readonly base58_encode: (a: number) => [number, number];
   readonly base58_decode: (a: number, b: number) => [number, number, number];
-  readonly base16_encode_lower: (a: number) => [number, number];
-  readonly base16_encode_upper: (a: number) => [number, number];
-  readonly base16_decode_mixed: (a: number, b: number) => [number, number, number];
-  readonly base16_decode_lower: (a: number, b: number) => [number, number, number];
-  readonly base16_decode_upper: (a: number, b: number) => [number, number, number];
   readonly __wbg_memory_free: (a: number, b: number) => void;
   readonly memory_new: (a: number, b: number) => number;
   readonly memory_ptr: (a: number) => number;
